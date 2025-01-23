b74979cdfdbbd36696bb15504d08ba226e7a3226
Steven Arthur <steven.arthur.degen@gmail.com>
2025-01-23 01:00:22 -0500

    created data.mjs to place services info

---
+++ src/data.mjs
@@ -0,0 +1,50 @@
+export const services = [
+  {
+    title: "Skimming Surface",
+    description:
+      "We carefully skim the surface of your pool to remove debris, ensuring your water stays clean, inviting, and ready to enjoy.",
+  },
+  {
+    title: "Brushing Pool",
+    description:
+      "We thoroughly brush your pool’s tiles and walls to remove any algae or buildup, keeping your pool spotless and well-maintained.",
+  },
+  {
+    title: "Vacuum",
+    description:
+      "Skimmer Vacuum: Effectively removes sand and dirt for a spotless finish. Self-Battery Vacuum: Cleans up larger debris from the pool bottom, ensuring a clean and clear swimming environment.",
+  },
+  {
+    title: "Filter Cleaning",
+    description:
+      "Regular filter cleaning is essential for maintaining bacteria-free water and ensuring a smooth, efficient flow throughout your pool system.",
+  },
+  {
+    title: "Empty Baskets",
+    description:
+      "We regularly empty your pool baskets to remove debris and ensure proper water flow to the system, keeping your pool running smoothly and efficiently.",
+  },
+  {
+    title: "Chemical Balance",
+    description:
+      "Proper chemical balancing not only keeps your pool water crystal clear but also protects the pool's finish, plumbing, and equipment from damage, ensuring long-lasting durability and performance.",
+  },
+];
+
+export const extraServices = [
+  {
+    title: "D.E Filter Breakdown",
+    description:
+      "An annual D.E. filter breakdown is essential to maintain optimal performance. Over time, D.E. filters can become clogged with debris and buildup, reducing efficiency. By thoroughly cleaning and reassembling the filter, we ensure your pool’s filtration system continues to deliver crystal-clear water and operates at peak efficiency.",
+  },
+  {
+    title: "Green to clean",
+    description:
+      "We transform your pool from green to sparkling clear in just 4 days, restoring its beauty and clarity quickly and effectivel",
+  },
+  {
+    title: "Repairs",
+    description:
+      "We specialize in pool pump and motor replacements, light fixture repairs, plumbing, and more, ensuring your pool stays in top condition year-round.",
+  },
+];
